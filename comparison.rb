6e80d1d044a7645d2900e98126c98e9acad41ab7
yuitoku <exrecord160@gmail.com>
2018-03-25 21:44:10 +0900

    Modify comparison.rb

--- comparison.rb
+++ comparison.rb
@@ -1,3 +1,21 @@
+# This class compare files in each directories.
+# And make file
 class Comparison
-  
+  def initialize(before_directory, after_directory, _none = nil)
+    @directory = [before_directory, after_directory]
+  end
+
+  def start
+    confirm_directory
+  end
+
+  def confirm_directory
+    files = []
+    @directory.each do |dir|
+      files.push(Dir.glob("*", base: dir).sort)
+    end
+  end
 end
+
+comparison = Comparison.new(*ARGV)
+comparison.start
